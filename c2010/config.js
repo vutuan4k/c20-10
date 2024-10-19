6669dfa1ba6c91147b40f27389e57bd9d4735456
vutuan4k <dungdienvuchi@gmail.com>
2024-10-19 22:43:38 +0700

    first commit

---
+++ c2010/config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Chúc cậu 20/10 vv';
+const giftUrl = 'https://www.facebook.com/ZhengYujunwu';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Lời gửi ngắm 20/10';
+const contentCard = 'Dù cậu mạnh mẽ hay dịu dàng, yểu điệu hay cá tính, thích trở thành cô giáo hay kỹ sư, sau này muốn lập gia đình sớm hay theo đuổi sự nghiệp... hãy cứ sống như trái tim mách bảo và làm điều bạn thực sự khao khát! Tớ chúc cậu mỗi ngày đều là 20/10 với thật nhiều niềm vui và luôn tỏa sáng'
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'VN.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
